7a851f3034173da864ad02384f2a0aacc494367c
Aditya Manglam Sharma <aditya300899@gmail.com>
2021-04-27 19:49:38 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,99 @@
+// const express = require("express");
+// const app = express();
+// const mysql = require('mysql');
+// const cors = require("cors");
+// const { request } = require("express");
+
+// app.use(cors);
+// app.use(express.json());
+
+
+// app.listen(3030, () => {
+//     console.log("It's working");
+// });
+ 
+const express = require("express");
+const app = express();
+// const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/add/patient/', (req, res) => {
+    console.log(request.body);
+    // res.send("All good");
+});
+
+
+app.post('/getdetails/', (req, res) => {
+    // res.send("All good");
+});
+
+// const db = mysql.createConnection({
+//   user: "root",
+//   host: "localhost",
+//   password: "password",
+//   database: "employeeSystem",
+// });
+
+// app.post("/create", (req, res) => {
+//   const name = req.body.name;
+//   const age = req.body.age;
+//   const country = req.body.country;
+//   const position = req.body.position;
+//   const wage = req.body.wage;
+
+//   db.query(
+//     "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+//     [name, age, country, position, wage],
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.send("Values Inserted");
+//       }
+//     }
+//   );
+// });
+
+// app.get("/employees", (req, res) => {
+//   db.query("SELECT * FROM employees", (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
+
+// app.put("/update", (req, res) => {
+//   const id = req.body.id;
+//   const wage = req.body.wage;
+//   db.query(
+//     "UPDATE employees SET wage = ? WHERE id = ?",
+//     [wage, id],
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.send(result);
+//       }
+//     }
+//   );
+// });
+
+// app.delete("/delete/:id", (req, res) => {
+//   const id = req.params.id;
+//   db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});
